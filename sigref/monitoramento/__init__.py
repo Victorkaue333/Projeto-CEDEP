007a4397ea5796dd777cb9da3c871520eb912b47
Victorkaue333 <kaue.alves.pg@gmail.com>
2025-05-09 13:18:58 -0300

    Update __init__.py

--- sigref/monitoramento/__init__.py
+++ sigref/monitoramento/__init__.py
@@ -1,13 +1,33 @@
-#.\venv\scripts\activate
+#python -m venv venv criar ambiente virtual
+#.\venv\scripts\activate ativar ambiente virtual
 
+#---------------------------------------------------
+#Django
+#iniciar o site
 #python manage.py runserver
+
 #python manage.py createsuperuser
+
+#---------------------------------------------------
+#Banco de dados
 #python manage.py makemigrations
 #python manage.py migrate
+
+#---------------------------------------------------
+#Testes automatizados
 #python manage.py test
+
+#---------------------------------------------------
+#Arquivos estáticos .css, .js, imagens,
 #python manage.py collectstatic
-#pip freeze > requirements.txt
+
+#---------------------------------------------------
+#Instalação primeira vez 
 #pip install -r requirements.txt
 
-#pip install pyinstaller
-#pyinstaller --onefile manage.py
+#---------------------------------------------------
+# Não usem na primeira vez
+#pip freeze > requirements.txt
+
+#---------------------------------------------------
+
